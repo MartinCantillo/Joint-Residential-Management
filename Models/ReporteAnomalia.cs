e4f2aa4812439d2f6e880e3921dea59dee717e5c
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-03-24 19:46:46 -0500

    Model of ReporteAnomalia

--- Models/ReporteAnomalia.cs
+++ Models/ReporteAnomalia.cs
@@ -1,8 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ModelResidente.Residente;
+
 namespace ModelsReporteAnomalias.ReporteAnomalias
 {
-   public class ReporteAnomalias{
-    private int Id{get;}
-   }
+    public class ReporteAnomalias
+    {
+        public ReporteAnomalias(string DescripcionAnomalia, string FechaReporteAnomalia,
+        string FotoAnomalia, string TipoAnomalia, string AsuntoAnomalia, Residente Residente)
+        {
+            this.DescripcionAnomalia = DescripcionAnomalia;
+            this.FechaReporteAnomalia = FechaReporteAnomalia;
+            this.FotoAnomalia = FotoAnomalia;
+            this.TipoAnomalia = TipoAnomalia;
+            this.AsuntoAnomalia = AsuntoAnomalia;
+            this.Residente = Residente;
+            Estados = ["Pendiente"];
+        }
 
-    
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int Id { get; }
+        [Required]
+        private string DescripcionAnomalia { set; get; }
+        [Required]
+        private string FechaReporteAnomalia { set; get; }
+        private string FotoAnomalia { set; get; }
+        [Required]
+        private string TipoAnomalia { set; get; }
+        [Required]
+        private string AsuntoAnomalia { set; get; }
+
+        [Required]
+        [ForeignKey("Id_residente")]
+        private Residente Residente { get; }
+        private ICollection<String> Estados { set; get; }
+
+
+    }
+
+
 }
