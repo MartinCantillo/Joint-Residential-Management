4dfaa80150b359c2b061a1100009e56d3f47a4ca
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-03-24 19:02:52 -0500

    model user

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelsUser.User
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int Id_User { set; get; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Username invalido")]
+        private string Username { set; get; }
+
+        [Required]
+        private string Password { set; get; }
+
+        private ICollection<String> Roles { set; get; }
+
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+            Roles = new HashSet<String>();
+        }
+    }
+}
