3990c5c894e86faed6fa2f1945f88bca66362eaf
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-03-25 10:22:58 -0500

    IUser

--- Repositories/IUser.cs
+++ Repositories/IUser.cs
@@ -1,7 +1,15 @@
+using ModelsUser.User;
+
 namespace RepositoriesIUser
 {
-    public interface IUser{
-        
+    public interface IUser
+    {
+        public Task SaveUser(User user);
+        public Task DeleteUser(int Iduser);
+        public Task<User> GetById(int idUser);
+        public Task<User> Update(int idUser);
+        public Task<ICollection<User>> GetAll();
+
     }
-    
+
 }
